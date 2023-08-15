642c814cf99c1ac600c0cec35fee91c4ef77b600
RavenDale <tds3143@gmail.com>
2023-08-14 18:44:12 -0700

    provide all information

--- index.js
+++ index.js
@@ -1,48 +1,86 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
-    apiKey: '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'
+    apiKey: ''
 });
 
 const openai = new OpenAIApi(config);
 
 const runPrompt = async () => {
+    const dishes_names = ["Omelette", "Quiche", "Egg Fried Rice"];
     const ingredients_prepared = ["Eggs", "Milk", "Salt", "Pepper", "Butter"];
-    const prompt = `Please recommend 5 dishes \
-    that can be prepared using the following ingredients:\n
+    const serving = 1;
+
+    const prompt = `Provide me with information regarding the dishes I've named and the prepared ingredients. \
+     \n
+    Dishes Names:\n
+    - ${dishes_names.join(', ')}\n
     Prepared ingredients:\n
     - ${ingredients_prepared.join(', ')}\n
-    You don't need to use all of the prepared ingredients. \
-    You can also suggest dishes that require 1 to 3 additional ingredients. \
-    If there are limited options with the prepared ingredients, \
-    you can suggest dishes that require more ingredients for a wider variety. \
-    You should give only the names of dishes. \
-    Don't provide the recipe and ingredients. \n`;
+    Serving size: ${serving}\n
+
+    To respond, format your answer as a JSON object, \
+    with "name", "serving", "ingredients", "time", "instructions" as the keys. \n
+    Put the cooking instructions for the specific dish in the correct sequential order in "instructions". \
+    The recipe should match the "Ingredients". and should be suitable for ${serving} serving(s).\
+    Don't number the first letter of the instructions. \n
+    "time" should be integer as minutes.
+    `;
 
     const appropriateExamples = `
-    Appropriate examples:
-    1. Spaghetti Carbonara
-    2. Chicken Tikka Masala
-    3. Beef Stir-Fry
-    4. Margherita Pizza
-    5. Chocolate Soufflé
-    `;
+    [
+        {
+          "name": "Omelette",
+          "serving": 1,
+          "ingredients": ["Eggs", "Milk", "Salt", "Pepper", "Butter"],
+          "time": 15,
+          "instructions": [
+            "In a bowl, whisk 2 eggs with a splash of milk. Season with a pinch of salt and pepper.",
+            "Heat a non-stick skillet over medium heat and melt a teaspoon of butter.",
+            "Pour the egg mixture into the skillet. Let it cook undisturbed for a minute.",
+            "Using a spatula, gently push the cooked edges toward the center, tilting the skillet to let the uncooked egg flow to the edges.",
+            "When the omelette is mostly set but still slightly runny on top, add your desired fillings on one half.",
+            "Fold the other half of the omelette over the fillings and cook for another minute.",
+            "Slide the omelette onto a plate and serve."
+          ]
+        },
+        {
+          "name": "Quiche",
+          "serving": 4,
+          "ingredients": ["Eggs", "Cream", "Pie crust", "Cheese", "Vegetables"],
+          "time": 60,
+          "instructions": [
+            "Preheat the oven to 375°F (190°C).",
+            "In a bowl, whisk together 4 eggs and 1 cup of heavy cream.",
+            "Roll out the pie crust and line a pie dish. Sprinkle cheese and chopped vegetables on the bottom.",
+            "Pour the egg mixture over the cheese and vegetables.",
+            "Bake in the preheated oven for 40-45 minutes, or until the quiche is set and golden.",
+            "Allow it to cool slightly before slicing and serving."
+          ]
+        },
+        {
+          "name": "Egg Fried Rice",
+          "serving": 2,
+          "ingredients": ["Cooked rice", "Eggs", "Vegetables", "Soy sauce", "Sesame oil"],
+          "time": 20,
+          "instructions": [
+            "Heat a wok or a large skillet over medium-high heat. Add a bit of oil.",
+            "Scramble 2 eggs in the wok. Once cooked, remove and set aside.",
+            "In the same wok, add more oil if needed. Add chopped vegetables and stir-fry until tender.",
+            "Add the cooked rice to the wok. Break up any clumps as it cooks.",
+            "Drizzle soy sauce and a bit of sesame oil over the rice. Mix well.",
+            "Add the scrambled eggs back to the wok and stir-fry for another minute.",
+            "Serve the egg fried rice hot."
+          ]
+        }
+      ]`;
 
-    const inappropriateExamples = `
-    Inappropriate examples:
-    1. Spaghetti Carbonara with Creamy Sauce
-    2. Chicken Tikka Masala with Basmati Rice
-    3. Beef Stir-Fry with Mixed Vegetables
-    4. Classic Margherita Pizza Recipe
-    5. Decadent Chocolate Soufflé with Raspberry Coulis
-    `;
-
-    const fullPrompt = prompt + appropriateExamples + inappropriateExamples;
+    const fullPrompt = prompt + appropriateExamples;
 
     const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: 'system', content: fullPrompt }],
-        max_tokens: 450,
+        max_tokens: 2500,
         temperature: 1,
     });
 
